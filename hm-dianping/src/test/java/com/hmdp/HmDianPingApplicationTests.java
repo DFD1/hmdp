ec4db92affa4778f73b5fe5cc94e0493c7b41316
DFD1 <1240047299@qq.com>
2023-03-26 20:57:43 +0800

    2023-3-26 实现全局ID生成器并测试

--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,43 @@
 package com.hmdp;
 
+import com.hmdp.utils.RedisIdWorker;
+import org.apache.ibatis.javassist.bytecode.analysis.Executor;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 @SpringBootTest
 class HmDianPingApplicationTests {
+    @Resource
+    private RedisIdWorker redisIdWorker;
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
+    private ExecutorService es = Executors.newFixedThreadPool(10);
+
+    @Test
+    void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(10);
+        Runnable task = () ->{
+            for (int i = 0; i < 10; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = "+id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 10; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = "+(end - begin));
+    }
 }
