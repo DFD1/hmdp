ce04ac56a879dfa1d01030069353941c2ed27bd9
DFD1 <1240047299@qq.com>
2023-04-19 16:31:39 +0800

    实现一人一单功能

--- hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -11,6 +11,7 @@ import com.hmdp.service.IVoucherService;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
 import org.apache.tomcat.jni.Local;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -33,7 +34,7 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
     @Resource
     private RedisIdWorker redisIdWorker;
     @Override
-    @Transactional
+
     public Result seckillVoucher(Long voucherId) {
         //1.查询优惠券信息
         SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
@@ -58,18 +59,38 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
             //扣减失败
             return Result.fail("库存不足");
         }
-        //6.创建订单
+        Long userId = UserHolder.getUser().getId();
+        synchronized (userId.toString().intern()){
+            //获取代理对象(事务)
+            IVoucherOrderService proxy = (IVoucherOrderService) AopContext.currentProxy();
+            return proxy.createVoucherOrder(voucherId);
+        }
+
+    }
+
+    @Transactional
+    public  Result createVoucherOrder(Long voucherId) {
+        //6.一人一单
+        Long userId = UserHolder.getUser().getId();
+        //6.1查询订单
+        int count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        //6.2判断是否存在
+        if(count > 0){
+            return Result.fail("用户已经购买过一次优惠券了");
+        }
+        //7.创建订单
         VoucherOrder voucherOrder = new VoucherOrder();
-        //6.1订单id
+        //7.1订单id
         long orderId = redisIdWorker.nextId("order");
         voucherOrder.setId(orderId);
-        //6.2用户id
-        Long userId = UserHolder.getUser().getId();
+        //7.2用户id
         voucherOrder.setUserId(userId);
-        //6.3代金券id
+        //7.3代金券id
         voucherOrder.setVoucherId(voucherId);
         save(voucherOrder);
-        //7.返回订单id
+        //8.返回订单id
         return Result.ok(orderId);
+
+
     }
 }
